18c08786443b0ed84cda1188915316f53ed8d128
jzbor <jzbor@posteo.net>
2019-07-01 21:28:49 +0200

    Initial commit with i3, vim, termite and more

---
+++ .config/rofi.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+
+bg_color=#902f343f 
+htext_color=#34be5b
+text_color=#f3f4f5
+
+rofi -show run -lines 3 -eh 2 -width 100 -padding 400 -opacity "95" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "Quicksand 16"
