657c035e4fb7d09e32e4608238b21345b63fbab1
jzbor <jzbor@posteo.net>
2019-08-28 18:56:49 +0200

    New colors for rofi

--- .config/rofi.sh
+++ .config/rofi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
 bg_color=#902f343f 
-htext_color=#6D68ED
-text_color=#f3f4f5
+htext_color=#fed7ae
+text_color=#bfbfa1
 
 rofi -show run -lines 3 -eh 2 -width 100 -padding 400 -opacity "95" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "Quicksand 16"
