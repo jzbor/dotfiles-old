5ef8645f9bfb827da6490a0c9928fb39fe2ec218
jzbor <jzbor@posteo.net>
2019-10-19 14:05:01 +0200

    Improved music controll: It now remembers your last used player

--- .config/polybar/scripts/music.sh
+++ .config/polybar/scripts/music.sh
@@ -1,6 +1,44 @@
 #!/usr/bin/env sh
 
-status=$(playerctl status 2> /dev/null)
+
+last_player="/tmp/.lastplayer"
+
+if [ -f "$last_player" ]; then
+    player="$(cat $last_player)"
+else
+    player="Lollypop,spotify"
+fi
+
+
+if [ "$(playerctl -p Lollypop status 2> /dev/null)" == "Playing" ]; then
+    player="Lollypop"
+    echo $player > $last_player
+elif [ "$(playerctl -p spotify status 2> /dev/null)" == "Playing" ]; then
+    player="spotify"
+    echo $player > $last_player
+fi
+
+
+status=$(playerctl -p $player status 2> /dev/null)
+
+
+if (( $# > 0 )); then
+    case $1 in
+	"play" | "play-pause")
+	    playerctl -p $player play-pause
+	    ;;
+	"pause")
+	    playerctl -p $player pause
+	    ;;
+	"prev" | "previous")
+	    playerctl -p $player previous
+	    ;;
+	"next")
+	    playerctl -p $player next
+	    ;;
+    esac
+fi
+
 
 if [ $? != 0 ] || [ "$status" == "Stopped" ]
 then
@@ -13,8 +51,8 @@ else
     else
 	pre_icon=
     fi
-    artist=$(playerctl metadata artist)
-    title=$(playerctl metadata title)
+    artist=$(playerctl -p $player metadata artist)
+    title=$(playerctl -p $player metadata title)
     if (( ${#artist} > 20 ))
     then
 	artist="${artist::17}..."
