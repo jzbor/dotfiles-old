6999232cf41e16c00b9dd54fd851928a4bef8b73
jzbor <jzbor@posteo.net>
2019-11-18 21:54:01 +0100

    Restructuring the repo for new build system

---
+++ build_configs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Idea:
+# 1. User chooses bricks in relevant order
+# 2. User chooses device
+# 3. Script saves config for later rebuild
+# 4. Script builds into given dir
+#   4.a if a .git folder exists the repo gets updated
+#   4.b if there is no such folder a repo gets initialised in the folder
+#   4.c The user wishes not to create a git repo
+#
+# * Create config file for behavior like automatic pushing, repo creation etc.
+
