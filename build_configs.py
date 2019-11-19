6865919756ef7cff7c981d50f9df4b70c3848266
jzbor <jzbor@posteo.net>
2019-11-19 15:48:01 +0100

    Initialized build_configs.py script

---
+++ build_configs.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+def get_available_bricks():
+    """Return all available bricks with clear assigned numbers (dict/list)"""
+    pass
+
+def get_available_devices():
+    """Return all available devices with clear assigned numbers (dict/list)"""
+    pass
+
+
+# From here on the methods are for workspace rather than help methods
+
+def choose_bricks():
+    """Present the user with a selection interface for the bricks
+    and return selected bricks in given order
+    """
+    pass
+
+def choose_device():
+    """Present the user with a selection interface for the devices
+    and return selected target device
+    """
+    pass
+
+def save_config(bricks, device, options):
+    """Save bricks, device and maybe even options to file for reuse"""
+    pass
+
+def build_to_dir(target_dir, git=false, push=false):
+    """Actually build the configuraion"""
+    pass
+
+if __name__ = "__main__":
+    pass
+
+
